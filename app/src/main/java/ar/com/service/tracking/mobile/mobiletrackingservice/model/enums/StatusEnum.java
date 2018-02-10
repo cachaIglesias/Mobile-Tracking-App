7ed23decdf8e075e5474bb62ce868394a805b44b
Mariano Iglesias Marchese <marianoiglesiasmarchese@gmail.com>
2018-02-10 11:16:21 -0300

    se crea enumarativo para indicar estado de ordenes

---
+++ app/src/main/java/ar/com/service/tracking/mobile/mobiletrackingservice/model/enums/StatusEnum.java
@@ -0,0 +1,14 @@
+package ar.com.service.tracking.mobile.mobiletrackingservice.model.enums;
+
+/**
+ * Created by miglesias on 09/01/18.
+ */
+
+public enum StatusEnum {
+
+    Enviado,
+    Suspendido,
+    Finalizado,
+    Cancelado;
+
+}
